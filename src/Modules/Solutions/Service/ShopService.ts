fbb3bc8ccdcbd1bcfbca1aca3ba3396f4fc710d6
Ulkar Orucova <ulkarorudjeva98@gmail.com>
2026-01-07 16:26:06 +0400

    update ShopService

--- src/Modules/Solutions/Service/ShopService.ts
+++ src/Modules/Solutions/Service/ShopService.ts
@@ -1,12 +1,11 @@
 import * as ShopProvider from "../Provider/ShopProvider";
 
 export class ShopService {
-
   static async productList() {
     return await ShopProvider.getProducts().then((res) => {
       return res.data.map((item) => ({
         ...item,
-        productImage: `${import.meta.env.VITE_IMAGE_URL}/${item.productImage}`,
+        productImage: `${item.productImage}`,
       }));
     });
   }
@@ -15,9 +14,7 @@ export class ShopService {
     return await ShopProvider.getProductDetails(id).then((res) => {
       return {
         ...res.data,
-        productImage: `${import.meta.env.VITE_IMAGE_URL}/${
-          res.data.productImage
-        }`,
+        productImage: `${res.data.productImage}`,
       };
     });
   }
